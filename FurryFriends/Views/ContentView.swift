eab33cf9fd0122ed90276580e0805d9a156a4e95
Nicole Huang <nhuang@lcs.on.ca>
2022-03-01 11:58:40 -0500

    When press the button. It will change the picture.

--- FurryFriends/Views/ContentView.swift
+++ FurryFriends/Views/ContentView.swift
@@ -13,9 +13,9 @@ struct ContentView: View {
     
     // Address for main image
     // Starts as a transparent pixel – until an address for an animal's image is set
-    @State var currentRightImage = URL(string: "https://www.russellgordon.ca/lcs/miscellaneous/transparent-pixel.png")!
-    
     @State var currentLeftImage = URL(string: "https://www.russellgordon.ca/lcs/miscellaneous/transparent-pixel.png")!
+    
+    @State var currentRightImage = URL(string: "https://www.russellgordon.ca/lcs/miscellaneous/transparent-pixel.png")!
     
     @State var currentDogImage: DogImage = DogImage(message: "https://www.russellgordon.ca/lcs/miscellaneous/transparent-pixel.png",
                                                     status: "success")
@@ -45,11 +45,15 @@ struct ContentView: View {
             HStack{
                 
                 VStack{
-                    RemoteImageView(fromURL: currentRightImage)
+                    RemoteImageView(fromURL: currentLeftImage)
                     
                     Button(action: {
                         
                         progressDog += 0.125
+                        
+                        Task{
+                            await loadNewDog()
+                        }
                         
                     },
                            label: {
@@ -67,10 +71,14 @@ struct ContentView: View {
                 
                 
                 VStack{
-                    RemoteImageView(fromURL: currentLeftImage)
+                    RemoteImageView(fromURL: currentRightImage)
                     
                     Button(action: {
                         progressCat += 0.125
+                        
+                        Task{
+                            await loadNewCat()
+                        }
                     },
                            label: {
                         Image("CatBotton")
@@ -101,6 +109,7 @@ struct ContentView: View {
                 Spacer()
             }
             
+            
             List{
                 Text("I like cat more")
                 
@@ -127,9 +136,9 @@ struct ContentView: View {
             
             // Replaces the transparent pixel image with an actual image of an animal
             // Adjust according to your preference ☺️
-            currentRightImage = URL(string: currentDogImage.message)!
+            currentLeftImage = URL(string: currentDogImage.message)!
             
-            currentLeftImage = URL(string: currentCatImage.file)!
+            currentRightImage = URL(string: currentCatImage.file)!
             
         }
         .navigationTitle("Furry Friends")
@@ -158,6 +167,8 @@ struct ContentView: View {
             
             currentDogAddedToFavorites = false
             
+            currentLeftImage = URL(string: currentDogImage.message)!
+            
         } catch {
             print("Could not retrieve or decode the JSON from endpoint.")
             
@@ -185,6 +196,8 @@ struct ContentView: View {
             currentCatImage = try JSONDecoder().decode(CatImage.self, from: data)
             
             currentCatAddedToFavorites = false
+            
+            currentRightImage = URL(string: currentCatImage.file)!
             
         } catch {
             print("Could not retrieve or decode the JSON from endpoint.")
